04c79aeba2189080dec55f1247f40bbc0fdb407c
PeeperInTheSpace <lollof94@gmail.com>
2022-09-01 11:30:16 +0200

    Creating files

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/*
+
+Palindroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+
+*/
+
+const parolaInserita = prompt('Inserisci una parola');
+
+const parolaPalindroma = checkPalindroma(parolaInserita);
+
+// Verifico se la parola è palindroma
+
+function checkPalindroma(text) {
+    console.log(text);
+
+    console.log(text[0]);
+    console.log(text.charAt(0));
+
+    let 
+}
